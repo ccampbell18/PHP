deaf58d71a0a147496345d5fa4a98b4e654ba38e
ccampbell18 <charles.campbell.12331@gmail.com>
2016-09-29 11:26:30 -0400

    Version .003

--- public_html/database.php
+++ public_html/database.php
@@ -1,13 +1,13 @@
 <?php
 include "connect.php";
 
-$name = $_GET['name'];
-$username = $_GET['username'];
-$password = $_GET['password'];
+$name = stripslashes($_GET['name']);
+$username = stripslashes($_GET['username']);
+$password = md5(stripslashes($_GET['password']));
 
 
-/*
-$query = "insert into users (name, username, password) values('$name', '$username', '$password')";
+
+/*$query = "insert into users (name, username, password) values('$name', '$username', '$password')";
 $result = mysql_query($query, $connection);
 if (!$result){
     echo "sorry";
@@ -18,6 +18,26 @@ else{
     echo "Your form has been submitted successfully!<br />";
 }*/
 
-$query = "select * from users";
-$result = @mysql_query($query,$connection);
+$query = "insert into users (name, username, password) values('$name', '$username', '$password')";
+$result = mysql_query($query, $connection);
+if(!$result){
+    echo "Sorry that name is taken, idiot.";
+    echo"<a href = 'username.php'>Back to register</a>";
+}
+else{
+    echo "Thanks for registering.";
+}
+
+/*echo "<table border='1'>";
+echo "<tr><th>Name</th><th>Username</th><th>Password</th></tr>";
+while (($row = mysql_fetch_assoc($result)) != FALSE){
+    echo "<tr><td>{$row['name']}</td>";
+    echo "<<td>{$row['username']}</td>";
+    echo "<td>{$row['password']}</td></tr>\n";
+}
+echo "</table>";
+
+mysql_free_result($result);
+mysql_close($connection);*/
+
 ?>
--- public_html/username.php
+++ public_html/username.php
@@ -1,9 +1,18 @@
 <?php
+include'header';
+echo "Click below to register <br />";
 echo "<form name = 'form' action = 'database.php' method = 'get'>";
 echo "Name <input type = 'text' name = 'name'><br />";
 echo "Username <input type = 'text' name = 'username'><br />";
 echo "Password <input type = 'password' name = 'password'><br />";
-echo "<input type = 'submit' value = 'Click to Submit'>";
+echo "<input type = 'submit' value = 'Click to Register'>";
+echo "</form>";
+
+echo "Click below to login <br />";
+echo "<form name = 'form' action = 'database2.php' method = 'get'>";
+echo "Username <input type = 'text' name = 'username'><br />";
+echo "Password <input type = 'text' name = 'password'><br />";
+echo "<input type = 'submit' value 'Click to Login'>";
 echo "</form>";
 
 ?>
